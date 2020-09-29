c9be33912e6f64d27e1079677b57328a80f40bcc
Asif-91 <asifchowdhury323@gmail.com>
2020-09-29 16:27:15 +0600

    finally done crud basic part

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const { ObjectId } = require('mongodb');
 const MongoClient = require('mongodb').MongoClient;
 const ObjectID = require('mongodb').ObjectID;
 
@@ -16,31 +17,48 @@ app.use(bodyParser.urlencoded({ extended: false }));
 
 
 app.get('/', (req, res) => {
-    res.sendFile(__dirname + '/index.html')
+  res.sendFile(__dirname + '/index.html')
 })
 
 client.connect(err => {
   const productCollection = client.db("organicdb").collection("products");
 
-app.get('/products', (req, res) => {
-    productCollection.find({}).limit(7)
-    .toArray( (err, documents) => {
+  app.get('/products', (req, res) => {
+    productCollection.find({})
+      .toArray((err, documents) => {
         res.send(documents);
-    })
-})
+      })
+  })
+
+  app.get('/product/:id', (req, res) => {
+    productCollection.find({ _id: ObjectId(req.params.id) })
+      .toArray((err, documents) => {
+        res.send(documents[0]);
+      })
+  })
 
   app.post("/addProduct", (req, res) => {
     const product = req.body;
     productCollection.insertOne(product)
-    .then(result => {
-        console.log('data added successfully');
-        res.send('success');
-    })    
-  })
-  app.delete('/delete/:id', (req, res) => {
-      productCollection.deleteOne({_id: ObjectID(req.params.id)})
       .then(result => {
-          console.log(result);
+        res.redirect('/');
+      })
+  })
+
+  app.patch('/update/:id', (req, res) => {
+    productCollection.updateOne({ _id: ObjectID(req.params.id)},
+      {
+        $set: { price: req.body.price, quantity: req.body.quantity }
+      })
+      .then(result => {
+        res.send(result.modifiedCount > 0);
+      })
+  })
+
+  app.delete('/delete/:id', (req, res) => {
+    productCollection.deleteOne({ _id: ObjectId(req.params.id)})
+      .then(result => {
+        res.send(result.deletedCount > 0);
       })
   })
 
